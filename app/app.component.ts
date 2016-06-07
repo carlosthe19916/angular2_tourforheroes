1a3f319ddbbba905863065d418c053b6e3fc11c8
carlosthe19916 <carlosthe19916@gmail.com>
2016-06-07 12:25:21 -0500

    before services

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,29 +1,20 @@
-import {Component} from '@angular/core';
-
-export class Hero {
-    id: number;
-    name: string;
-}
-
+import { Component } from '@angular/core';
+import { Hero } from './hero';
+import { HeroDetailComponent } from './hero-detail.component';
 @Component({
     selector: 'my-app',
     template:`
-            <h2>My Heroes</h2>
-            <ul class="heroes">
-              <li *ngFor="let hero of heroes" (click)="onSelect(hero)" [class.selected] = "hero === selectedHero">
-                <span class="badge">{{hero.id}}</span> {{hero.name}}
-              </li>
-            </ul>
-            
-            <div *ngIf="selectedHero">
-                <h2>{{selectedHero.name}} details!</h2>
-                <div><label>id: </label>{{selectedHero.id}}</div>
-                <div>
-                    <label>name: </label>
-                    <input [(ngModel)]="selectedHero.name" placeholder="name"/>
-                </div>
-            </div>,
-            `,
+    <h1>{{title}}</h1>
+    <h2>My Heroes</h2>
+    <ul class="heroes">
+      <li *ngFor="let hero of heroes"
+        [class.selected]="hero === selectedHero"
+        (click)="onSelect(hero)">
+        <span class="badge">{{hero.id}}</span> {{hero.name}}
+      </li>
+    </ul>
+    <my-hero-detail [hero]="selectedHero"></my-hero-detail>
+  `,
     styles:[`
     .selected {
       background-color: #CFD8DC !important;
@@ -72,17 +63,15 @@ export class Hero {
       margin-right: .8em;
       border-radius: 4px 0 0 4px;
     }
-  `]
+  `],
+    directives: [HeroDetailComponent]
 })
 export class AppComponent {
-    title = 'Tour of Herores';
+    title = 'Tour of Heroes';
+    heroes = HEROES;
     selectedHero: Hero;
-    onSelect(hero: Hero) {
-        this.selectedHero = hero;
-    }
-    public heroes = HEROES;
+    onSelect(hero: Hero) { this.selectedHero = hero; }
 }
-
 var HEROES: Hero[] = [
     { "id": 11, "name": "Mr. Nice" },
     { "id": 12, "name": "Narco" },
@@ -95,4 +84,3 @@ var HEROES: Hero[] = [
     { "id": 19, "name": "Magma" },
     { "id": 20, "name": "Tornado" }
 ];
-
